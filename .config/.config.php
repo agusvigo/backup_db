7f3f623553f7bc8a9c00902e7a663d65442e80bd
Agustín Ferreiroa González <40137464+agusvigo@users.noreply.github.com>
2023-05-23 18:55:00 +0200

    Create .config.php

---
+++ .config/.config.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Define days to keep backup data */
+define("APP_CLEAN_DAYS", 30);
+
+/* Database connection values */
+define("DB_HOST", "localhost");
+define("DB", "you_db_name");
+define("DB_USER", "your_db_user");
+define("DB_PASS", "your_db_pwd");
+define("DB_PORT",3306);
+
+/* Version */
+$version = 'v1.2';
+?>
